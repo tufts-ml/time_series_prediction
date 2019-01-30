0655b555078711a2a619a1d2088e0bd6b9dc5b16
Byron Perpetua <byronperpetua@gmail.com>
2019-01-30 10:41:06 -0500

    Added dictionary for MIMIC demo TS data

--- src/normalize_features.py
+++ src/normalize_features.py
@@ -2,9 +2,10 @@
 
 # Input:  a file with unnormalized feature values and a data dictionary for
 #         that file, specified as command line parameters
-# Output: normalized.csv, with normalized feature values. Booleans (as specified
-#         by the data dictionary) and columns ending in '_id' are left alone.
-#         Scaling is based on each column's IQR for robustness to outliers.
+# Output: normalized.csv, with normalized feature values. Only fields of type
+#         'integer' or 'number' (as specified in the data dictionary) that do
+#         not end in '_id' are normalized. Scaling is based on each column's
+#         IQR for robustness to outliers.
 
 import sys
 import json
@@ -16,7 +17,7 @@ data_dict = json.load(open(sys.argv[2]))
 
 for x in data_dict['fields']:
     col = x['name']
-    if col[-3:] != '_id' and x['type'] != 'boolean':
+    if col[-3:] != '_id' and x['type'] in ('integer', 'number'):
         df[col] = robust_scale(df[col])
 
 df.to_csv('normalized.csv', index=False)
