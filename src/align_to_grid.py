2f56b0dd65de0ed6d0e7dbea95a319bd0008315c
Byron Perpetua <byronperpetua@gmail.com>
2019-02-05 15:49:05 -0500

    align_to_grid.py handles sequential (non-timestamped) data

--- src/align_to_grid.py
+++ src/align_to_grid.py
@@ -1,13 +1,20 @@
 # align_to_grid.py
 
-# Input:  a time-series file with irregular timesteps given in the 'time'
-#         column, specified as the first command line parameter, and one or more
-#         columns ending in '_id'
-# Output: aligned.csv, a time-series file with regular timesteps corresponding
-#         to the second command line parameter, grouped by all columns ending
-#         in '_id' and with values representing the mean for the given group
-#         within the given time window. For timestep syntax see
-#     http://pandas.pydata.org/pandas-docs/stable/timeseries.html#offset-aliases
+# Input:  a time-series file, specified as the first command line parameter,
+#         with one or more columns ending in '_id', and one of:
+#           - a 'time' column containing irregular timesteps
+#           - a 'seq_num' column containing the numbers 1, ..., n for each group
+# Output: aligned.csv, a time-series file with regular steps grouped by all
+#         columns ending in '_id'. The steps are determined by the second
+#         command line parameter. If there is a
+#           - 'time' column: the steps correspond to the syntax in
+#             http://pandas.pydata.org/pandas-docs/stable
+#             /timeseries.html#offset-aliases
+#           - 'seq_num' column: the steps are every n entries in the sequence,
+#             where n is the parameter
+#         Values represent the mean for the given group within each step. Steps
+#         are closed and labeled on the right: e.g., a step labeled 2:00:00
+#         might cover data from 1:00:01 to 2:00:00.
 
 # TODO: summary stat other than mean?
 
@@ -15,15 +22,20 @@ import sys
 import pandas as pd
 
 df = pd.read_csv(sys.argv[1])
-
-df['time2'] = pd.to_datetime(df['time'])
-df = df.drop('time', axis='columns')
-df = df.rename({'time2': 'time'}, axis='columns')
-
 group_cols = list(df.columns[df.columns.str.endswith('_id')])
 
-aligned = df.groupby(group_cols).resample(sys.argv[2], on='time', label='right',
-                                          closed='left').mean()
-aligned = aligned.drop(group_cols, axis='columns')
+if 'time' in df.columns:
+    df['_time'] = pd.to_datetime(df['time'])
+    df = df.drop('time', axis='columns')
+    df = df.rename({'_time': 'time'}, axis='columns')
+    aligned = df.groupby(group_cols).resample(sys.argv[2], on='time', label='right',
+                                            closed='right').mean()
+    aligned = aligned.drop(group_cols, axis='columns')
+elif 'seq_num' in df.columns:
+    length = int(sys.argv[2])
+    df['seq_num'] = df['seq_num'].apply(lambda x: length*(1+int((x-1)/length)))
+    aligned = df.groupby(group_cols + ['seq_num']).mean()
+else:
+    raise Exception('File must contain a "time" or "seq_num" column')
 
 aligned.to_csv('aligned.csv')
