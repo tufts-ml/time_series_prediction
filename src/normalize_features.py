fb24e9903621f66930a644b2dd18535d3371bebb
Byron Perpetua <byronperpetua@gmail.com>
2019-02-11 13:24:53 -0500

    Improved command line arguments; used data dictionary instead of column names; split EEG data into time-series and static

--- src/normalize_features.py
+++ src/normalize_features.py
@@ -1,24 +1,35 @@
 # normalized_features.py
 
-# Input:  a file with unnormalized feature values and a data dictionary for
-#         that file, specified as command line parameters
-# Output: normalized.csv, with normalized feature values. Only fields of type
-#         'integer' or 'number' (as specified in the data dictionary) that do
-#         not end in '_id' are normalized. Scaling is based on each column's
+# Input:  Argument 1: a time-series file with unnormalized feature values
+#         Argument 2: data dictionary for that flie
+#         Argument 3: output file path
+# Output: A time-series file with normalized feature values. Only fields of 
+#         role 'input' and type 'integer' or 'number' (as specified in the data
+#         dictionary) are normalized. Scaling is based on each column's
 #         IQR for robustness to outliers.
 
 import sys
 import json
 import pandas as pd
+import argparse
 from sklearn.preprocessing import robust_scale
 
-df = pd.read_csv(sys.argv[1])
-data_dict = json.load(open(sys.argv[2]))
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file')
+parser.add_argument('data_dict')
+parser.add_argument('output_file')
+args = parser.parse_args()
 
+# Import data
+df = pd.read_csv(args.input_file)
+data_dict = json.load(open(args.data_dict))
+
+# Normalize features
 for x in data_dict['fields']:
     col = x['name']
-    if (col[-3:] != '_id' and col != 'seq_num'
-            and x['type'] in ('integer', 'number')):
+    if (x['role'] == 'input' and x['type'] in ('integer', 'number')):
         df[col] = robust_scale(df[col])
 
-df.to_csv('normalized.csv', index=False)
+# Export data
+df.to_csv(args.output_file, index=False)
